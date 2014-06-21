1a3d8a44a23126f0ca9f101b88b570d615e18573
Enoch Riese <enoch.riese@gmail.com>
2014-06-21 16:48:48 -0400

    cross origin gem?

--- restroom_scrape.rb
+++ restroom_scrape.rb
@@ -7,7 +7,16 @@ require "sinatra/reloader"
 # state = gets.chomp
 # puts "What city are you in?"
 # city = gets.chomp
+require 'sinatra/cross_origin'
 
+configure do
+  enable :cross_origin
+end
+set :allow_origin, :any
+set :allow_methods, [:get, :post, :options]
+set :allow_credentials, true
+set :max_age, "1728000"
+set :expose_headers, ['Content-Type']
 get "/search_bathrooms.json" do
   content_type :json
   if params[:lat]

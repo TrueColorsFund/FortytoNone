481b99a995c281bf8f82b0c0f5d0127fd2c744b9
anne-decusatis <annie.decusatis@gmail.com>
2014-06-21 15:06:24 -0400

    added schema for json

---
+++ json.js
@@ -0,0 +1,11 @@
+ {
+    "name": "", // .itemName
+    "location": "", // .itemStreet
+	"rating" : 0, // .itemRating, might also be "not yet rated"
+	"distance" : 0, // .itemDistance
+    "accessible": true, // .not_accessible or nothing
+    "unisex": true, // .not_unisex or nothing
+	//below: things we want but refuge restrooms doesn't provide
+	"paid": true,
+	"welcoming": true //welcoming to homeless people?
+}
